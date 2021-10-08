4994f2edaa2b28d2333174826543566911fcf8db
tkawabat <tosiki.kawabata@gmail.com>
2021-10-08 23:49:05 +0900

    fix はじめの表示崩れ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import { AppProps } from 'next/app'
+import { StylesProvider } from "@mui/styles"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <StylesProvider injectFirst>
+            <Component {...pageProps} />
+        </StylesProvider>
+    )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets as MaterialUIStyleSheets } from '@mui/styles'
+import { ServerStyleSheet as StyledComponentsStyleSheets } from "styled-components"
 
 export default class MyDocument extends Document {
     render() {
@@ -39,5 +41,34 @@ export default class MyDocument extends Document {
                 </body>
             </Html>
         )
+    }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+    const materialUISheets = new MaterialUIStyleSheets()
+    const styledComponentsSheets = new StyledComponentsStyleSheets()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) => styledComponentsSheets.collectStyles(
+                    materialUISheets.collect(<App {...props} />)
+                ),
+            })
+
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    {styledComponentsSheets.getStyleElement()}
+                </>
+            ),
+        }
+    } finally {
+        styledComponentsSheets.seal()
     }
 }
