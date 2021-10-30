6ffab6aca18a0e095da50117ac461c0c10d3e601
tkawabat <tosiki.kawabata@gmail.com>
2021-10-30 17:54:25 +0900

    viewportを戻した

--- src/components/l5/HakogakiHead.tsx
+++ src/components/l5/HakogakiHead.tsx
@@ -14,7 +14,6 @@ const App = (props: Props) => {
     return (
         <Head>
             <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width;initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
             <link rel="apple-touch-icon" href="/logo192.png" />
             <link rel="manifest" href="/manifest.json" />
