b9fdd6420107e584250a43d4488793857e50a678
tkawabat <tosiki.kawabata@gmail.com>
2021-11-06 21:39:35 +0900

    fix smartphone header

--- src/components/l4/Header/smartphone.tsx
+++ src/components/l4/Header/smartphone.tsx
@@ -8,14 +8,7 @@ import * as C from '../../../lib/Const'
 import { pc, sp} from '../../../lib/Responsive'
 
 import GoogleButton from '../../l2/GoogleButton'
-import Center from '../../l1/Center'
-import Left from '../../l1/Left'
-import Right from '../../l1/Right'
-import ScenarioConfigModeChip from '../../l1/ScenarioConfigModeChip'
-import DeleteScenarioButton from '../../l1/DeleteScenarioButton'
-import FileButton from '../../l1/FileButton'
-import HelpButton from '../../l1/HelpButton'
-import ScenarioTitle from '../../l1/ScenarioTitle'
+import ScenarioTitle from 'src/components/l1/ScenarioTitle'
 
 const Root = styled.div`
     ${sp`
@@ -24,25 +17,23 @@ const Root = styled.div`
     ${pc`
         display: none;
     `}
+
+    display: flex;
+    align-items: center;
+    justify-items: center;
     
     width: 100%;
     height: 50px;
+    padding-left: 10px;
+    padding-right: 5px;
     background-color: #d7e6ef;
 `
 
-const ToolTitle = styled.span`
-    font-family: 'Helvetica Neue', 'Helvetica', 'Hiragino Sans', 'Hiragino Kaku Gothic ProN',
-        'Arial', 'Yu Gothic', 'Meiryo', sans-serif;
-    font-size: 20px;
-    font-weight: bold;
-`
-
-const StyledLeft = styled(Left)`
-    padding-left: 20px;
-`
-
-const StyledRight = styled(Right)`
-    padding-right: 20px;
+const Icon = styled.img`
+    display: flex;
+    width: 32px;
+    height: 32px;
+    margin-right: 10px;
 `
 
 const App = () => {
@@ -50,16 +41,9 @@ const App = () => {
 
     return (
         <Root className="header">
-            <StyledLeft>
-                <ScenarioTitle title={title} />
-            </StyledLeft>
-            <StyledRight>
-                <ScenarioConfigModeChip />
-                <DeleteScenarioButton />
-                <FileButton />
-                <GoogleButton />
-                <HelpButton />
-            </StyledRight>
+            <Icon src={'/logo192.png'} />
+            <ScenarioTitle title={title} />
+            <GoogleButton />
         </Root>
     )
 }
